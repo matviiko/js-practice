239d75c7e98e49ac9ed18e92d328ee8e8bb1c87a
matviiko <matviy.kopchak@gmail.com>
2020-11-05 16:33:59 +0200

    create note object if form validate

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 import './style.css';
+import { isValid } from './utils';
 
 const form = document.getElementById('form');
+const input = form.querySelector('#note-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value);
+});
+
+function submitFormHandler(event) {
+  event.preventDefault();
+
+  if (isValid(input.value)) {
+    const note = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    submitBtn.disabled = true;
+
+    // Async request to server to save notes
+    console.log('Notes', note);
+
+    input.value = '';
+    input.classList = '';
+    submitBtn.disabled = false;
+  }
+}
